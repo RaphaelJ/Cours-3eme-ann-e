e4d54dc89b39eecc476a9c24e4c7be14fbf20d3c
Raphael Javaux <raphaeljavaux@gmail.com>
2011-09-20 15:53:47 +0200

    first commit

---
+++ SGBD/InpreZon/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import patterns, include, url
+
+import views
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^livres/$', views.livres, name='livres'),    
+    
+    #url(r'^$', 'InpreZon.views.home', name='home'),
+    
+    # url(r'^InpreZon/', include('InpreZon.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+
---
+++ SGBD/InpreZon/views/__init__.py
@@ -0,0 +1,1 @@
+from livres import *
