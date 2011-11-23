a1881404e8ca958a64f44c6a0ace445548961de3
Raphael Javaux <raphaeljavaux@gmail.com>
2011-11-24 00:33:49 +0100

    alim indep

--- SGBD/InpresZon/alimentation_indep.fs
+++ SGBD/InpresZon/alimentation_indep.fs
@@ -2,14 +2,19 @@ open System
 open System.Net
 open System.Text.RegularExpressions
 
-let (fnac_livres:Printf.StringFormat<_>) = "http://livre.fnac.com/l898/Meilleures-ventes-Livre?sl=-1.0&PageIndex=%d"
+let (fnac_livres:Printf.StringFormat<_>) = "http://livre.fnac.com/l898/Meilleur\
+es-ventes-Livre?sl=-1.0&PageIndex=%d"
 
 let DownloadString (url:String) = (new WebClient()).DownloadString(url)
 let ExtractLinks content =
     let expr =
-        "http://livre.fnac.com/l898/Meilleures-ventes-Livre?SID="
-    for n in Regex.Matches(content, expr) do
-        printfn "%s" content.[n.Index..n.Index+350]
+        "http://livre.fnac.com/l898/Meilleures-ventes-Livre\?SID=.{36}\
+&UID=.{37}&Origin=FnacAff&OrderInSession=0&TTL=\d+&sl=-1.0&PageIndex=(\d+)\
+&ItemPerPage=15"
+    
+    for m in Regex.Matches(content, expr) do
+        printfn "%s" (m.Groups.[1].Value)
 
-do Console.WriteLine(sprintf fnac_livres 1 |> DownloadString |> ExtractLinks)
+    Regex.Matches(content, expr)
 
+do printfn "Count: %d" (sprintf fnac_livres 1 |> DownloadString |> ExtractLinks).Count
