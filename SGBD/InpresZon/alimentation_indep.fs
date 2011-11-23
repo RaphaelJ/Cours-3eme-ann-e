3911734dc737d2fe3802192c9117ab3800674a1d
Raphael Javaux <raphaeljavaux@gmail.com>
2011-11-23 13:35:42 +0100

    alimentation_indep

--- SGBD/InpresZon/alimentation_indep.fs
+++ SGBD/InpresZon/alimentation_indep.fs
@@ -1,8 +1,15 @@
 open System
-open Control.WebExtensions
+open System.Net
+open System.Text.RegularExpressions
 
 let (fnac_livres:Printf.StringFormat<_>) = "http://livre.fnac.com/l898/Meilleures-ventes-Livre?sl=-1.0&PageIndex=%d"
 
-let DownloadString = new WebClient().DownloadString
+let DownloadString (url:String) = (new WebClient()).DownloadString(url)
+let ExtractLinks content =
+    let expr =
+        "http://livre.fnac.com/l898/Meilleures-ventes-Livre?SID="
+    for n in Regex.Matches(content, expr) do
+        printfn "%s" content.[n.Index..n.Index+350]
 
-do Console.WriteLine(sprintf fnac_livres 1 |> DownloadString)
+do Console.WriteLine(sprintf fnac_livres 1 |> DownloadString |> ExtractLinks)
+
