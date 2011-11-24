b4fc1735d5fbbd1d2da3047bf0d37658028373f8
Raphael Javaux <raphaeljavaux@gmail.com>
2011-11-24 16:35:47 +0100

    listing movies musics

--- SGBD/InpresZon/alimentation_indep.fs
+++ SGBD/InpresZon/alimentation_indep.fs
@@ -2,19 +2,59 @@ open System
 open System.Net
 open System.Text.RegularExpressions
 
-let (fnac_livres:Printf.StringFormat<_>) = "http://livre.fnac.com/l898/Meilleur\
-es-ventes-Livre?sl=-1.0&PageIndex=%d"
+type MediaSource =
+    { Listing: Printf.StringFormat<int -> string>;
+      PageExpr: string;
+      ArticleExpr: string }
 
-let DownloadString (url:String) = (new WebClient()).DownloadString(url)
-let ExtractLinks content =
-    let expr =
-        "http://livre.fnac.com/l898/Meilleures-ventes-Livre\?SID=.{36}\
-&UID=.{37}&Origin=FnacAff&OrderInSession=0&TTL=\d+&sl=-1.0&PageIndex=(\d+)\
+let books = {
+      Listing="http://livre.fnac.com/l898/Meilleures-ventes-Livre?sl=-1.0\
+&PageIndex=%d"
+    ; PageExpr="http://livre.fnac.com/l898/Meilleures-ventes-Livre\\?SID=.{36}\
+&UID=.{37}&Origin=FnacAff&OrderInSession=0&TTL=\\d+&sl=-1.0&PageIndex=(\\d+)\
 &ItemPerPage=15"
-    
-    for m in Regex.Matches(content, expr) do
-        printfn "%s" (m.Groups.[1].Value)
+    ; ArticleExpr="http://livre.fnac.com/a\d+/" }
 
-    Regex.Matches(content, expr)
+let musics = {
+      Listing="http://musique.fnac.com/l827/Meilleures-ventes-musique?sl=-1.0\
+&PageIndex=%d"
+    ; PageExpr="http://musique.fnac.com/l827/Meilleures-ventes-musique\\?SID\
+=.{36}&UID=.{37}&Origin=FnacAff&OrderInSession=0&TTL=\\d+&sl=-1.0&PageIndex=\
+(\\d+)&ItemPerPage=15"
+    ; ArticleExpr="http://musique.fnac.com/a\d+/" }
 
-do printfn "Count: %d" (sprintf fnac_livres 1 |> DownloadString |> ExtractLinks).Count
+let movies = {
+      Listing="http://video.fnac.com/l6484/Meilleures-ventes-DVD-et-Blu-Ray\
+?sl=-1.0&PageIndex=%d"
+    ; PageExpr="http://video.fnac.com/l6484/Meilleures-ventes-DVD-et-Blu-Ray\
+\\?SID=.{36}&UID=.{37}&Origin=FnacAff&OrderInSession=0&TTL=\\d+&sl=-1.0\
+&PageIndex=(\\d+)&ItemPerPage=15"
+    ;  ArticleExpr="http://video.fnac.com/a\d+/" }
+
+let downloadString (url: String) = (new WebClient()).DownloadString(url)
+
+/// Extrait les liens vers les pages des articles les plus populaires
+let extractListing media =
+    let downloadListing iPage = sprintf media.Listing iPage |> downloadString
+    let firstPage = downloadListing 1
+    // Récupère le nombre de pages de listing
+    let pagesMatches = Regex.Matches(firstPage, media.PageExpr)
+    let nPages = seq { for m in pagesMatches-> Int32.Parse(m.Groups.[1].Value) }
+                 |> Seq.max
+    printfn "%d pages" nPages
+    // Récupère le contenu de toutes les pages de listing
+    let pagesContents =
+        seq { for p in seq {2..nPages} -> downloadListing p }
+        |> Seq.append (Seq.singleton firstPage)
+    // Récupère les liens vers les articles du listing
+    let articlesMatches content = Regex.Matches(content, media.ArticleExpr)
+    let articlesLinks content =
+        seq { for a in articlesMatches content -> a.Value }
+    pagesContents |> Seq.map articlesLinks |> Seq.concat |> Set.ofSeq 
+
+do let articles = extractListing books
+                  |> Seq.append (extractListing musics)
+                  |> Seq.append (extractListing movies)
+   for l in articles do
+        printfn "%s" l
+   printfn "%d articles" (articles |> Seq.toList |> List.length)
