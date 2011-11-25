5ba2a55f3adff6151ebdbf02f63edfe27c5b9457
Raphael Javaux <raphaeljavaux@gmail.com>
2011-11-25 15:31:15 +0100

    protocol serveur checkin

--- Java/FerryInpres/src/data_bean/Accompagnant.java
+++ Java/FerryInpres/src/data_bean/Accompagnant.java
@@ -9,6 +9,88 @@ package data_bean;
  * @author rapha
  */
 public class Accompagnant {
-    private i
-    
+    private int _id;
+    private String _nom;
+    private String _prenom;
+    private String _adresse;
+    private int _voyageur;
+
+    public Accompagnant(int _id, String _nom, String _prenom, String _adresse,
+            int _voyageur) {
+        this._id = _id;
+        this._nom = _nom;
+        this._prenom = _prenom;
+        this._adresse = _adresse;
+        this._voyageur = _voyageur;
+    }
+
+    /**
+     * @return the _id
+     */
+    public int getId() {
+        return _id;
+    }
+
+    /**
+     * @param id the _id to set
+     */
+    public void setId(int id) {
+        this._id = id;
+    }
+
+    /**
+     * @return the _nom
+     */
+    public String getNom() {
+        return _nom;
+    }
+
+    /**
+     * @param nom the _nom to set
+     */
+    public void setNom(String nom) {
+        this._nom = nom;
+    }
+
+    /**
+     * @return the _prenom
+     */
+    public String getPrenom() {
+        return _prenom;
+    }
+
+    /**
+     * @param prenom the _prenom to set
+     */
+    public void setPrenom(String prenom) {
+        this._prenom = prenom;
+    }
+
+    /**
+     * @return the _adresse
+     */
+    public String getAdresse() {
+        return _adresse;
+    }
+
+    /**
+     * @param adresse the _adresse to set
+     */
+    public void setAdresse(String adresse) {
+        this._adresse = adresse;
+    }
+
+    /**
+     * @return the _voyageur
+     */
+    public int getVoyageur() {
+        return _voyageur;
+    }
+
+    /**
+     * @param voyageur the _voyageur to set
+     */
+    public void setVoyageur(int voyageur) {
+        this._voyageur = voyageur;
+    }
 }
