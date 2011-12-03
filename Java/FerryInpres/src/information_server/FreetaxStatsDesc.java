d7ae2044bafae669db619ef2d66d9474a90be127
Raphael Javaux <raphaeljavaux@gmail.com>
2011-12-03 17:43:10 +0100

    serveur stats

--- Java/FerryInpres/src/information_server/FreetaxStatsDesc.java
+++ Java/FerryInpres/src/information_server/FreetaxStatsDesc.java
@@ -9,5 +9,55 @@ package information_server;
  * @author rapha
  */
 public class FreetaxStatsDesc implements FreetaxStatsProtocol {
-    
+    private String _categorie;
+    private Integer _mois;
+    private Integer _semaine;
+
+    public FreetaxStatsDesc(String _categorie, Integer _mois, Integer _semaine) {
+        this._categorie = _categorie;
+        this._mois = _mois;
+        this._semaine = _semaine;
+    }
+
+    /**
+     * @return the _categorie
+     */
+    public String getCategorie() {
+        return _categorie;
+    }
+
+    /**
+     * @param categorie the _categorie to set
+     */
+    public void setCategorie(String categorie) {
+        this._categorie = categorie;
+    }
+
+    /**
+     * @return the _mois
+     */
+    public Integer getMois() {
+        return _mois;
+    }
+
+    /**
+     * @param mois the _mois to set
+     */
+    public void setMois(Integer mois) {
+        this._mois = mois;
+    }
+
+    /**
+     * @return the _semaine
+     */
+    public Integer getSemaine() {
+        return _semaine;
+    }
+
+    /**
+     * @param semaine the _semaine to set
+     */
+    public void setSemaine(Integer semaine) {
+        this._semaine = semaine;
+    }
 }
